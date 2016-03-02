b4b46d142f672a6a5514e7918a0097c662bfec97
mallotore <jmarinasll@gmail.com>
2016-03-03 00:28:25 +0100

    fix path

--- vm/windows-server-2008-IIS7/scripts/install-java.ps1
+++ vm/windows-server-2008-IIS7/scripts/install-java.ps1
@@ -1,4 +1,4 @@
 echo "Installing java"
-$Install = Start-Process "C:\vagrant_data\software\jdk-7u79-windows-x64.exe" -ArgumentList "/s" -Wait 
+$Install = Start-Process "C:\vagrant_shared\software\jdk-7u79-windows-x64.exe" -ArgumentList "/s" -Wait 
 Write-Host "New Version of Java is Installing..."
 write-host "Successfully installed"
