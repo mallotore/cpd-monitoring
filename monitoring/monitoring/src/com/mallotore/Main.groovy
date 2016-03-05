bc8fabc30aec81cc49e26421c1dcf2d20d516730
mallotore <jmarinasll@gmail.com>
2016-03-05 13:54:45 +0100

    using long max value

--- monitoring/monitoring/src/com/mallotore/Main.groovy
+++ monitoring/monitoring/src/com/mallotore/Main.groovy
@@ -5,9 +5,11 @@ import com.mallotore.monitoring.jmx.JmxBeanExporter
 class Main {
     
     public static void main(String[] args) throws Exception {
+        
         new JmxBeanExporter().export()
+        
         while(true){
-           Thread.sleep(200000); 
+           Thread.sleep(Long.MAX_VALUE); 
         }
     }
 }
