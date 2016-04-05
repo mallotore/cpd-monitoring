99c264bf91931a1f6d532902f5b21e68ba2777ec
mallotore <jmarinasll@gmail.com>
2016-04-06 01:50:31 +0200

    actualizado script de creacion de tablas

--- web/cpd/bd-scripts/CPD.sql
+++ web/cpd/bd-scripts/CPD.sql
@@ -7,8 +7,14 @@ CREATE SCHEMA cpd ;
 CREATE TABLE  `cpd`.`server_configuration` (
   `name` varchar(43) NOT NULL,
   `ip` varchar(43) NOT NULL,
-  `port` varchar(10) NOT NULL,
-  `service` varchar(150) DEFAULT NULL,
-  `uuid` varchar(43) NOT NULL
+  `port` int NOT NULL,
+  `probe_interval_in_seconds` int NOT NULL DEFAULT 0,
+  `uuid` varchar(43) NOT NULL,
   PRIMARY KEY (`uuid`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE  `cpd`.`temperature_configuration` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `probe_interval_in_seconds` int NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
