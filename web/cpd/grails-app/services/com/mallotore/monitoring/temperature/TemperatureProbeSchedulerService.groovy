d6f8991eddfe8ba70342bf10f91fd307d6689d45
mallotore <jmarinasll@gmail.com>
2016-04-10 15:46:29 +0200

    convirtiendo a milisegundos que es lo que usa el api de arduino

--- web/cpd/grails-app/services/com/mallotore/monitoring/temperature/TemperatureProbeSchedulerService.groovy
+++ web/cpd/grails-app/services/com/mallotore/monitoring/temperature/TemperatureProbeSchedulerService.groovy
@@ -14,16 +14,17 @@ class TemperatureProbeSchedulerService{
 	def temperatureRepository
 
 	def schedule(intervalInSeconds){
+		def intervalInMilliSeconds = convertToMilliseconds(intervalInSeconds)
 		if(!temperatureReader) {
-			temperatureReader = new TemperatureReader(temperatureRepository, intervalInSeconds)
+			temperatureReader = new TemperatureReader(temperatureRepository, intervalInMilliSeconds)
 		}else{
 			temperatureReader.close()	
 		}
-		temperatureReader.updateInterval(intervalInSeconds)
+		temperatureReader.updateInterval(intervalInMilliSeconds)
 		temperatureReader.initialize()
 		task {
 			Thread.sleep(MINIMUM_TIME_TO_WAIT_TO_BE_READY_ARDUIN0)
-			temperatureReader.writeData("${intervalInSeconds}")
+			temperatureReader.writeData("${intervalInMilliSeconds}")
 		}
 	}
 
@@ -31,5 +32,9 @@ class TemperatureProbeSchedulerService{
 		if(temperatureReader){
 			temperatureReader.close()
 		}
+	}
+
+	private convertToMilliseconds(intervalInSeconds){
+		intervalInSeconds * 1000
 	}
 }
