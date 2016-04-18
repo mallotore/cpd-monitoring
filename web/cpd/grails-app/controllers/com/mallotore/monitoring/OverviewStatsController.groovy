1fd298ed8dcad807119be1bdde9a85480b81c54f
mallotore <jmarinasll@gmail.com>
2016-04-18 18:52:18 +0200

    green * finds server stats overview * finds temperature overview stats

--- web/cpd/grails-app/controllers/com/mallotore/monitoring/OverviewStatsController.groovy
+++ web/cpd/grails-app/controllers/com/mallotore/monitoring/OverviewStatsController.groovy
@@ -1,10 +1,40 @@
 package com.mallotore.monitoring
 
+import com.mallotore.configuration.dto.ServerDto
+import grails.converters.JSON
+
 class OverviewStatsController {
 
-    static allowedMethods = [home: "GET"]
+	def serverConfigurationService
+	def temperatureRepository
+	def serverStatsRepository
+
+    static allowedMethods = [home: "GET", findServerStatsOverviewByIp: "GET", findTemperatureStatsOverview: "GET", ]
 
     def home() { 
-        render view:'/monitoring/overview'
+    	def servers = serverConfigurationService.findAllServers()
+    	def serversDto = servers?.collect{
+            new ServerDto(id: it.id,
+                        name: it.name,
+                        ip:it.ip,
+                        port: it.port,
+                        probeInterval: it.probeIntervalInSeconds,
+                        connectivityAlert: it.connectivityAlert,
+                        diskPercentageAlert: it.diskPercentageAlert)
+        }
+
+        render view:'/monitoring/overview', model: [servers: servers]
+    }
+
+    def findServerStatsOverviewByIp(String ip) { 
+    	def stats = serverStatsRepository.findLastByIp(ip)
+
+    	render([serverStats: stats.state()] as JSON)
+    }
+
+    def findTemperatureStatsOverview(){
+    	def stats = temperatureRepository.findLast()
+
+    	render([temperatureStats: stats.state()] as JSON)
     }
 }
--- web/cpd/src/test/groovy/com/mallotore/monitoring/OverviewStatsControllerSpec.groovy
+++ web/cpd/src/test/groovy/com/mallotore/monitoring/OverviewStatsControllerSpec.groovy
@@ -2,20 +2,59 @@ package com.mallotore.monitoring
 
 import grails.test.mixin.TestFor
 import spock.lang.Specification
-import com.mallotore.monitoring.jmx.*
-import com.mallotore.monitoring.jmx.dto.*
+import com.mallotore.configuration.ServerConfiguration
+import com.mallotore.configuration.ServerConfigurationService
+import com.mallotore.configuration.dto.ServerDto
+import com.mallotore.monitoring.repository.TemperatureRepository
+import com.mallotore.monitoring.repository.ServerStatsRepository
+import com.mallotore.monitoring.model.ServerStats
+import com.mallotore.monitoring.model.ServerStatsState
+import com.mallotore.monitoring.model.Temperature
 
 @TestFor(OverviewStatsController)
 class OverviewStatsControllerSpec extends Specification {
 
     def setup() {
+        controller.serverConfigurationService = Mock(ServerConfigurationService)
+        controller.temperatureRepository = Mock(TemperatureRepository)
+        controller.serverStatsRepository = Mock(ServerStatsRepository)
     }
 
     def "shows overview"() {
         when:
+        request.method = 'GET'
         controller.home()
 
         then:
+        1 * controller.serverConfigurationService.findAllServers() >> [
+            new ServerConfiguration(id: "uuid", name: "Sql-1", ip: "127.1.1.1", port: 1617, probeIntervalInSeconds: 60)
+        ]
         view == '/monitoring/overview'
+        model.servers.size() == 1
+        model.servers[0].id == "uuid"
+    }
+
+    def "finds server stats overview"(){
+        when:
+        request.method = 'GET'
+        controller.findServerStatsOverviewByIp("127.0.0.1")
+
+        then:
+        1 * controller
+            .serverStatsRepository
+            .findLastByIp("127.0.0.1") >> new ServerStats(new ServerStatsState(ip: "127.0.0.1"))
+        response.json.serverStats.ip == '127.0.0.1'
+    }
+
+    def "finds temperature overview stats"(){
+        when:
+        request.method = 'GET'
+        controller.findTemperatureStatsOverview()
+
+        then:
+        1 * controller.temperatureRepository.findLast() >> new Temperature(_id: "id", 
+                                                              temperature: "20",
+                                                              creationDate: new Date())
+        response.json.temperatureStats.temperature == "20"
     }
 }
